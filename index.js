0124dc820028b3929faa421b2ff56a36dd4654e0
Asinus Grandus <91527907+AsinusGrandus@users.noreply.github.com>
2023-07-07 19:09:34 +0200

    Stopped text from moving

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const LAST_MESSAGE_DATE = new Date('July 22, 2021 21:38:00');
+const LAST_MESSAGE_DATE = new Date('July 22, 2021 21:38:21');
 const audio = new Audio('assets/outro-meme.mp3');
 let isPlaying = false;
 
 class Timer{
-    constructor(dt, number){
+    constructor(dt, n){
         this.dt = dt;
-        this.years = document.querySelectorAll(".years")[number];
-        this.days = document.querySelectorAll(".days")[number];
-        this.hours = document.querySelectorAll(".hours")[number];
-        this.minutes = document.querySelectorAll(".minutes")[number];
-        this.seconds = document.querySelectorAll(".seconds")[number];
+        this.n = n;
+
+        this.years = document.querySelectorAll(".years");
+        this.days = document.querySelectorAll(".days");
+        this.hours = document.querySelectorAll(".hours");
+        this.minutes = document.querySelectorAll(".minutes");
+        this.seconds = document.querySelectorAll(".seconds");
     }
 
     updateDisplay(purge){
@@ -19,11 +21,21 @@ class Timer{
         const MINUTES = Math.floor(this.dt / 60) % 60;
         const SECONDS = Math.floor(this.dt) % 60;
 
-        this.years.innerText = (purge && YEARS == 0) ? "" : `${YEARS} ${YEARS != 1 ? "years" : "year"}`;
-        this.days.innerText = (purge && DAYS == 0) ? "" : `${DAYS < 10 ? "0" + DAYS: DAYS} ${DAYS != 1 ? "days" : "day" }`;
-        this.hours.innerText = (purge && HOURS == 0 && DAYS == 0) ? "" : `${HOURS < 10 ? "0" + HOURS: HOURS} ${HOURS != 1 ? "hours" : "hour" }`;
-        this.minutes.innerText = `${MINUTES < 10 ? "0" + MINUTES: MINUTES} ${MINUTES != 1 ? "minutes": "minute" }`;
-        this.seconds.innerText = `${SECONDS < 10 ? "0" + SECONDS: SECONDS} ${SECONDS != 1 ? "seconds" : "second" }`;
+
+        this.years[this.n].innerText = (purge && YEARS == 0) ? "" : `${YEARS}`;
+        this.years[this.n+1].innerText = (purge && YEARS == 0) ? "" : `${YEARS != 1 ? "years" : "year"}`;
+
+        this.days[this.n].innerText = (purge && DAYS == 0) ? "" : `${DAYS}`;
+        this.days[this.n+1].innerText = (purge && DAYS == 0) ? "" : `${DAYS != 1 ? "days" : "day" }`;
+
+        this.hours[this.n].innerText = (purge && HOURS == 0 && DAYS == 0) ? "" : `${HOURS < 10 ? "0" + HOURS: HOURS}`;
+        this.hours[this.n+1].innerText = (purge && HOURS == 0 && DAYS == 0) ? "" : `${HOURS != 1 ? "hours" : "hour" }`;
+
+        this.minutes[this.n].innerText = `${MINUTES < 10 ? "0" + MINUTES: MINUTES}`;
+        this.minutes[this.n+1].innerText = `${MINUTES != 1 ? "minutes": "minute" }`;
+
+        this.seconds[this.n].innerText = `${SECONDS < 10 ? "0" + SECONDS: SECONDS}`;
+        this.seconds[this.n+1].innerText = `${SECONDS != 1 ? "seconds" : "second" }`;
     }
 }
 
@@ -31,22 +43,23 @@ const interval = setInterval(() => {
     const isBefore = new Date().setFullYear(LAST_MESSAGE_DATE.getFullYear()) < LAST_MESSAGE_DATE;
     const factor = isBefore ? 0: 1;
     const yearssince = new Date().getFullYear() - LAST_MESSAGE_DATE.getFullYear() + factor;
-    const anniversary = new Date(`July 22, ${LAST_MESSAGE_DATE.getFullYear() + yearssince} 21:38:00`);
+    const anniversary = new Date(`July 22, ${LAST_MESSAGE_DATE.getFullYear() + yearssince} 21:38:21`);
 
     const until = document.getElementById("until");
-    until.innerText = `Time until ${yearssince} ${yearssince == 1 ? 'st': yearssince == 2 ? 'nd': 'th'} anniversary`;
+    until.innerText = `Time until ${yearssince}${yearssince == 1 ? 'st': yearssince == 2 ? 'nd': 'th'} anniversary`;
 
     if (((anniversary - new Date()) / 1000) <= 10 && !isPlaying){
         audio.play();
         isPlaying = true; // Play the audio once. We assume the user refreshes the site before next anniversary.
-    } 
-    if (((anniversary - new Date()) / 1000) <= 1){
-        console.log('confetti')
-        const jsConfetti = new JSConfetti()
-        jsConfetti.addConfetti()
     }
 
-    const countdown = new Timer((anniversary - new Date()) / 1000, 1);
+    if (((anniversary - new Date()) / 1000) <= 1){
+        console.log('confetti')
+        const jsConfetti = new JSConfetti();
+        jsConfetti.addConfetti();
+    }
+
+    const countdown = new Timer((anniversary - new Date()) / 1000, 2);
     const timer = new Timer((new Date() - LAST_MESSAGE_DATE) / 1000, 0);
 
     countdown.updateDisplay(false);
